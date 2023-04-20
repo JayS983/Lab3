bb45f871c396a6ab7c8874a0e00cebcb02622d55
JayS <krazynik016@gmail.com>
2023-04-20 20:08:30 +0300

    ListsScreen.kt

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ListsScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ListsScreen.kt
@@ -1,12 +1,26 @@
 package com.raywenderlich.android.lab1.screens
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
-import java.lang.reflect.Modifier
 
 private val items=listOf(
     BookCategory(
@@ -52,11 +66,43 @@ fun ListScreen(){
 
 @Composable
 fun MyList(){
-
+LazyColumn{
+    items(items){item -> ListItem(item)}
+}
 }
 
+
+
 @Composable
-fun ListItem(bookCategory:BookCategory,modifier: Modifier){
+fun ListItem(bookCategory: BookCategory){
+
+    Column(modifier= androidx.compose.ui.Modifier.padding(8.dp)){
+        Text(
+            text= stringResource(bookCategory.categoryResourceId),
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            color= colorResource(id = R.color.colorPrimary)
+
+        )
+        val modifier = null
+        Spacer(modifier = modifier!!.height(8.dp))
+        LazyRow{
+            items(bookCategory.bookImageResources){ items->
+                BookImage(items)
+
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun BookImage(ImageResource:Int) {
+    Image(modifier = androidx.compose.ui.Modifier.size(170.dp, 220.dp),
+        painter = painterResource(id = ImageResource),
+        contentScale= ContentScale.Fit,
+        contentDescription = stringResource(id = R.string.books))
 
 }
 
